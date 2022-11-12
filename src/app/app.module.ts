dffe4e4a93b598cfe0b4b298fa5cf8e5b3ac5ed5
JoseXS <xskunk@gmail.com>
2022-11-12 18:53:05 +0100

    feat: Se añade pwa basica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppComponent } from './app.component';
 import { NgxPermissionsModule } from 'ngx-permissions';
 import { initializerProviders } from '@shared/services/system/initializer.providers';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,6 +22,12 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
     NgxPermissionsModule.forRoot(),
     AngularSvgIconModule.forRoot(),
     AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [
     // { provide: LoginService, useClass: FakeLoginService }, // <= Remove it in the real APP

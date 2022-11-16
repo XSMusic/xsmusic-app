3a34fb8da6848fd83cc1f9c44199de04a8531fc3
JoseXS <xskunk@gmail.com>
2022-11-16 16:34:02 +0100

    feat: Mejoras para modo oscuro

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -43,14 +43,14 @@ export class MapComponent implements AfterViewInit {
 
   addTiles() {
     const tiles = L.tileLayer(
-      'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+      'https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
       {
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
       }
     );
     const googleHybrid = L.tileLayer(
-      'http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
+      'https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
       {
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],

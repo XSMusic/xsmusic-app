c78e9baa4524a48d2e74b8c9082c4eccd47dce4f
JoseXS <xskunk@gmail.com>
2022-12-01 02:11:24 +0100

    fix: Correcciones

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -99,7 +99,11 @@ export class MapComponent implements AfterViewInit {
   getDarkMode() {
     return L.tileLayer(
       'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
-      { maxZoom: 20 }
+      {
+        maxZoom: 20,
+        attribution:
+          '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+      }
     );
   }
 

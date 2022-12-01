6a1b3d64e43d4a8861b0f4a5b9b034a2588fcc77
JoseXS <xskunk@gmail.com>
2022-12-01 10:21:10 +0100

    feat: Se añade api a stadiamaps (leaflet)

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,25 +1,22 @@
 /*eslint-disable */
 const { writeFile } = require('fs');
 const { argv } = require('yargs');
-// read environment variables from .env file
 require('dotenv').config();
-// read the command line arguments passed with yargs
+
 const environment = argv.environment;
 const isProduction = environment === 'prod';
 const targetPath = isProduction
   ? `./src/environments/environment.prod.ts`
   : `./src/environments/environment.ts`;
-// we have access to our environment variables
-// in the process.env object thanks to dotenv
 const environmentFileContent = `
-export const environment = {
-   production: ${isProduction},
-   API_URL: "${process.env['API_URL']}",
-   IMAGES_URL: "${process.env['IMAGES_URL']}",
-   APP_URL: "${process.env['APP_URL']}",
-};
+  export const environment = {
+    production: ${isProduction},
+    API_URL: "${process.env['API_URL']}",
+    IMAGES_URL: "${process.env['IMAGES_URL']}",
+    APP_URL: "${process.env['APP_URL']}",
+    API_STADIAMAPS: "${process.env['API_STADIAMAPS']}",
+  };
 `;
-// write the content to the respective file
 writeFile(targetPath, environmentFileContent, function (err: any) {
   if (err) {
     console.error(err);
--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -98,12 +98,8 @@ export class MapComponent implements AfterViewInit {
 
   getDarkMode() {
     return L.tileLayer(
-      'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
-      {
-        maxZoom: 20,
-        attribution:
-          '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-      }
+      `https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png&api_key=${environment.API_STADIAMAPS}`,
+      { maxZoom: 20 }
     );
   }
 
